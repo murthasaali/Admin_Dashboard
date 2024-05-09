6c562bd4101b1dc7f79000d87990dc7c9c1c632e
murthasaali <murthasaalick123@gmail.com>
2024-05-09 21:33:09 +0530

    fea : "updated ui of topBuyes and Admin nav bar"

--- src/pages/AdiminDashboard.jsx
+++ src/pages/AdiminDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CiSearch } from "react-icons/ci";
+import { CiInstagram, CiSearch } from "react-icons/ci";
 import { FaRegBell, FaSearch } from "react-icons/fa";
 import { CiMail } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
@@ -54,40 +54,30 @@ function AdminDashboard() {
      {
       !isOrder? <div className="max-w-[70%]  w-[70%] p-px bg-gradient-to-b h-full  bg-opacity-10 flex-col flex gap-3 ">
       <nav className="w-full flex justify-start gap-2 items-center text-xs">
-        <div className="p-px bg-gradient-to-b from-customColor2  opacity-70 to-pink-300 dark:from-from-blue-300 dark:to-from-blue-300  rounded-full">
-          <div className="w-[300px] h-10 bg-gradient-to-b from-customColor2 rounded-full to-customColor  flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
+          <div className="w-[300px] h-10  rounded-full placeholder:text-white  bg-stone-50 bg-opacity-10 flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
             <CiSearch className="text-white" />
             <input
               type="text"
-              className="bg-transparent outline-none w-full "
+              className="bg-transparent text-white text-xs outline-none w-full "
             />
-          </div>
         </div>
  
-        <div className="p-px bg-gradient-to-b from-customColor2 opacity-70 to-pink-300 dark:from-from-blue-300 dark:to-from-blue-300  rounded-full">
-          <div className="w-fit h-10 bg-gradient-to-b font-bold text-white from-customColor2 rounded-full to-customColor  flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
-            <CiSearch className="text-white" />
+          <div className="w-fit h-10 bg-stone-50 bg-opacity-10 rounded-lg font-bold text-white  flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
+            <CiInstagram className="text-white" />
             32,ETH
 
        
           </div>
-        </div>
-        <div className="p-px bg-gradient-to-b from-customColor2 opacity-70 to-pink-300 dark:from-from-blue-300 dark:to-from-blue-300  rounded-full">
-          <div className="w-fit h-10 bg-gradient-to-b font-bold text-white from-customColor2 rounded-full to-customColor  flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
+          <div className="w-fit h-10 bg-stone-100 bg-opacity-10 font-bold text-white rounded-full   flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
         <FaRegBell/>
        
-          </div>
         </div>
-        <div className="p-px bg-gradient-to-b from-customColor2 opacity-70 to-pink-300 dark:from-from-blue-300 dark:to-from-blue-300  rounded-full">
-          <div className="w-fit h-10 bg-gradient-to-b font-normal text-white from-customColor2 rounded-full to-customColor  flex justify-center items-center text-xl p-4 gap-2 shadow-md  ">
+          <div className="w-fit h-10 bg-stone-100 rounded-full bg-opacity-10 font-normal text-xs text-white   flex justify-center items-center  p-4 gap-2 shadow-md  ">
         Create
           </div>
-        </div>
-        <div className="p-px bg-gradient-to-b from-customColor2 opacity-70 to-pink-300 dark:from-from-blue-300 dark:to-from-blue-300  rounded-full">
-          <div className="w-fit h-10 bg-gradient-to-b font-thin text-white from-customColor2 rounded-full to-customColor  flex justify-center items-center  p-4 gap-2 shadow-md  ">
+          <div className="w-fit h-10 bg-stone-100 bg-opacity-10 font-thin text-white  rounded-full   flex justify-center items-center  p-4 gap-2 shadow-md  ">
         connect wallet
           </div>
-        </div>
         <div className="p-px h-full bg-stone-50"></div>
         <div className="p-px h-ful flex gap-2">
           {/* <div className="w-10 h-10 rounded-full bg-slate-600" style={{backgroundImage:"url('https://avatars.mds.yandex.net/i?id=628c908972b034f7236d7d6c63b12119de955a63-9569150-images-thumbs&ref=rim&n=33&w=375&h=375')"}}></div> */}
--- src/pages/components/TopBuyersList.jsx
+++ src/pages/components/TopBuyersList.jsx
@@ -10,12 +10,20 @@ top Buyers                <button className='p-2'>
                 <BsThreeDotsVertical className=' text-iconcolor'/>
                 </button>
             </nav>
-            <div className="grid grid-cols-1 h-[100%] overflow-y-auto gap-2 p-4">
+            <div className="grid grid-cols-1 h-[100%] overflow-y-hidden gap-2 p-2">
 {
   topBuyers.map((item,index)=>(
 
-    <div key={index}  className="h-16 w-full bg-stone-100 bg-opacity-10 justify-start items-center flex p-2 rounded-lg">
+    <div key={index}  className="h-[70px] w-full bg-stone-100 hover:bg-opacity-80 text-stone-400 hover:text-black hover:scale-105 transition-all duration-500 bg-opacity-10 justify-start items-center flex p-3 rounded-lg gap-2">
+      <div className=''>
       <div className='w-10 h-10 rounded-lg' style={{backgroundImage:`url(${item.image})` ,backgroundSize:"cover ", backgroundPosition:"center"}}></div>
+      <h1 className='text-[10px]  font-thin'>{item.name}</h1>
+      </div>
+      <div className='flex justify-between flex-col h-full'>
+        <h1 className='text-[10px]'>highest sales :<span className='text-red-400 text-[15px]'>{item.totalSale}</span></h1>
+        <h1 className='text-xs '>{item.totalTax}%</h1>
+      </div>
+
     </div>
   ))
 
