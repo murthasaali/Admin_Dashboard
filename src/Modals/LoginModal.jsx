3a09d18de01366d6fe4b72d1992d00be36406ddb
murthasaali <murthasaalick123@gmail.com>
2024-05-15 17:19:06 +0530

    fea : "logal modal updated"

--- src/Modals/LoginModal.jsx
+++ src/Modals/LoginModal.jsx
@@ -26,7 +26,7 @@ const LoginModal = () => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-90 z-50">
-      <div className="bg-stone-100 bg-opacity-45 p-4 rounded-lg shadow-md flex flex-col  " style={{backgroundImage:"url('')"}}>
+      <div className="bg-stone-100 bg-opacity-45 p-4 rounded-3xl shadow-md flex flex-col  " style={{backgroundImage:"url('')"}}>
         <h2 className="text-2xl font-semibold mb-4">Admin Login</h2>
         <form onSubmit={handleSubmit} className="flex flex-col w-[300px] gap-2 ">
           <label htmlFor="username  "  className="text-xs"> Username:</label>
